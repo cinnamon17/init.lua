7e082a67e7cc58c1b321e927ce67d46c4b90672b
Nelson <juniormoncada17@gmail.com>
2022-12-30 22:16:54 +0100

    config neovim

--- lua/theprimeagen/packer.lua
+++ lua/theprimeagen/packer.lua
@@ -21,6 +21,12 @@ return require('packer').startup(function(use)
 	  end
   })
 
+
+  use {
+      'nvim-lualine/lualine.nvim',
+      requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+
   use({'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'})
   use('nvim-treesitter/playground')
   use('theprimeagen/harpoon')
